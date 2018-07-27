b80e57b59bb180937ca1e0d2b40476f955c4cda7
Josh Curry <joshuacurry2615@gmail.com>
2018-07-26 23:33:50 -0400

    Deleted some commented code

--- ReTweet.py
+++ ReTweet.py
@@ -10,38 +10,10 @@ access_token_secret = 'yEPSAcJkzJsKMVnhpfm2eqPTo54MoZyTJlm4SwZgUG067'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
 
-person = api.user_timeline(screenname = 'Curry2615')
-
-public_tweets = api.home_timeline()
-
-test = [
-    "potato",
-    "pie",
-    "1+1+1"
-]
-# my_file=open('sample20.txt','r')
-# file_lines=my_file.readlines()
-# my_file.close()
-
-
-
-# for line in test:
-# # Add try ... except block to catch and output errors
-#     try:
-#         print(line)
-#         if line != '\n':
-#             #this line tweets the input
-#             api.send_direct_message(person)
-#         else:
-#             pass
-#     except tweepy.TweepError as e:
-#         print(e.reason)
-#     sleep(5)
 count = 0
-for tweet in tweepy.Cursor(api.search, q='#soccer').items():
+for tweet in tweepy.Cursor(api.search, q='#corgi').items():
   
     try:
         # Add \n escape character to print() to organize tweets
@@ -54,7 +26,6 @@ for tweet in tweepy.Cursor(api.search, q='#soccer').items():
         count += 1
         if count == 5:
             break
-        sleep(5)
     
     except tweepy.TweepError as e:
         print(e.reason)
